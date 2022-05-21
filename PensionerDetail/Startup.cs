2a05e91e79f143899511cd0e2026cb1d10bc8304
Venkatesh Kumar <venkateshkumartuty@gmail.com>
2022-05-21 23:01:45 +0530

    changes in swagger

--- PensionerDetail/Startup.cs
+++ PensionerDetail/Startup.cs
@@ -64,11 +64,12 @@ namespace PensionerDetail
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
+                
             }
 
             app.UseHttpsRedirection();
