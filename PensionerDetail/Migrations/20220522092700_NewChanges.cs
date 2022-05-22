b5f98954e5ef2488c0228a6fdeadd2cd2c06f773
Venkatesh Kumar <venkateshkumartuty@gmail.com>
2022-05-22 15:04:04 +0530

    changes

---
+++ PensionerDetail/Migrations/20220522092700_NewChanges.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PensionerDetail.Migrations
+{
+    public partial class NewChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
